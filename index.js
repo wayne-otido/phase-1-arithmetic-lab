0a263af7214529b3c8fbb05b478325c593dd6353
Wayne Otido <otidowayne@gmail.com>
2023-06-14 14:39:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+let num1 = 1
+let num2 =62
+let multiply = num1*num2
+console.log(multiply)
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+let num3 = 26
+let num4 = 11
+let mod = num3 % num4
+console.log(mod)
+let max = Math.max(3, 6, 9, 20)
+console.log(max)
